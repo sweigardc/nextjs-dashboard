9aae7f02cc75fc67aac42d88692e34e73e30fc70
sweigardc <sweigardc@gmail.com>
2025-04-06 17:13:15 -0400

    Complete tutorial with database and auth implemented

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
+Styled using [Tailwind](https://tailwindcss.com). Continous Deployment using [Vercel](https://nextjs-dashboard-chris-sweigards-projects.vercel.app/).
+
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers, fetchCustomers } from '@/app/lib/data';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = fetchCustomers()
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query + currentPage}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        
+      </div>
+    </div>
+  );
 }
